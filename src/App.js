975d18e3402e633f24941b0037f5cc1b035c75b1
veerabv <bveera0608@gmail.com>
2024-07-24 08:50:33 +0530

    handling the http req in slice using a thunk function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 import Notification from "./components/UI/Notification";
 
 let initial = true;
@@ -16,49 +16,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.ShowNotification({
-          status: "pending",
-          title: "Sending",
-          message: "sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://react-dummy-8e08f-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to send the data ");
-      }
-
-      // const data = await response.json();
-      dispatch(
-        uiActions.ShowNotification({
-          status: "success",
-          title: "Success..!",
-          message: "sending cart data Successfully",
-        })
-      );
-    };
-
     if (initial) {
       initial = false; //this is used to skip the api call for the first time
       return;
     }
-    sendCartData().catch((err) => {
-      dispatch(
-        uiActions.ShowNotification({
-          status: "error",
-          title: "Error",
-          message: "sending cart data failed",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
